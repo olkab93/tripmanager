69b1e83f984e174036d56ee58ba2167d8e90948a
student6 <student6@pcoip.ki.agh.edu.pl>
2018-03-17 15:39:31 +0100

    TripManagerTest test templates added

--- src/test/java/pl/edu/agh/mwo/TripManagerTest.java
+++ src/test/java/pl/edu/agh/mwo/TripManagerTest.java
@@ -1,6 +1,28 @@
 package pl.edu.agh.mwo;
- 
+
+import org.junit.Test;
+
 public class TripManagerTest {
+	
+	@Test
+	public void addTripTest() {
+		// TO DO
+	}
+	
+	@Test
+	public void removeTripTest() {
+		// TO DO
+	}
+	
+	@Test
+	public void getTripsTest() {
+		// TO DO
+	}
+	
+	@Test
+	public void findTripTest() {
+		// TO DO
+	}
 	
 
 }
