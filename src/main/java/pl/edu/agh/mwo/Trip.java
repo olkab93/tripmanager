6b5d64bad4279ef0906bad4877cdbe94d7cbf282
olkab <olka.bochenska93@gmail.com>
2018-04-10 13:39:36 +0200

    trip name and description added. Getters and setters generated.

--- src/main/java/pl/edu/agh/mwo/Trip.java
+++ src/main/java/pl/edu/agh/mwo/Trip.java
@@ -5,6 +5,25 @@ import java.util.Collection;
 
 public class Trip {
 
+	String name;
+	String description;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 	Collection<Photo> photos = new ArrayList<Photo>();
 	
 	public Collection<Photo> getPhotos() {
