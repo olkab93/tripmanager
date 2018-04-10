2ba42170ec69467e388939850c34874f5c629e5e
olkab <olka.bochenska93@gmail.com>
2018-04-10 14:26:03 +0200

    addTrip() added to TripManager

--- src/main/java/pl/edu/agh/mwo/TripManager.java
+++ src/main/java/pl/edu/agh/mwo/TripManager.java
@@ -1,6 +1,19 @@
 package pl.edu.agh.mwo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class TripManager {
+
+    Collection<Trip> trips = new ArrayList<Trip>();
+
+    public void addTrip(Trip trip) {
+        trips.add(trip);
+    }
+
+    public Collection<Trip> getTrips() {
+        return trips;
+    }
 
 
 }
