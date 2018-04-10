74f9dc41d116687ff75ca9c4d333cd6411bc543d
olkab <olka.bochenska93@gmail.com>
2018-04-10 13:11:04 +0200

    createDataForTests() in TripTest added

--- src/test/java/pl/edu/agh/mwo/TripTest.java
+++ src/test/java/pl/edu/agh/mwo/TripTest.java
@@ -2,14 +2,21 @@ package pl.edu.agh.mwo;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TripTest {
+    private Trip trip;
+    private Photo photo;
+
+    @Before
+    public void createDataForTests() {
+        trip = new Trip();
+        photo = new Photo();
+    }
 
 	@Test
 	public void addPhoto() {
-		Trip trip = new Trip();
-		Photo photo = new Photo();
 		assertEquals(0, trip.getPhotos().size());
 		trip.addPhoto(photo);
 		assertEquals(1, trip.getPhotos().size());
