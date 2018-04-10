4a348d4503c87cf20b772975d5b1cafd58343fd8
olkab <olka.bochenska93@gmail.com>
2018-04-10 14:00:42 +0200

    Photo comment added. Setter and getter generated.

--- src/main/java/pl/edu/agh/mwo/Photo.java
+++ src/main/java/pl/edu/agh/mwo/Photo.java
@@ -2,4 +2,13 @@ package pl.edu.agh.mwo;
 
 public class Photo {
 
+    String comment;
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
